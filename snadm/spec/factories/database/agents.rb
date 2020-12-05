3a1ab29de376c46c43ccf00b5c6297f0c95d0aac
Pathe SENE <psene@boaholding.com>
2020-12-05 01:18:42 +0000

    Add basic factories

--- snadm/spec/factories/database/agents.rb
+++ snadm/spec/factories/database/agents.rb
@@ -1,17 +1,6 @@
 FactoryBot.define do
   factory :database_agent, class: 'Database::Agent' do
-    identifiant { "MyString" }
-    mot_de_passe_crypte { "MyString" }
+    sequence(:identifiant) { |n| 'agent-#{n}' }
+    mot_de_passe_crypte { 'MyString' }
   end
 end
-
-# == Schema Information
-#
-# Table name: database_agents
-#
-#  id                  :bigint           not null, primary key
-#  identifiant         :string
-#  mot_de_passe_crypte :string
-#  created_at          :datetime         not null
-#  updated_at          :datetime         not null
-#
